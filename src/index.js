edf45e7b74d2a65493aefca882e87b05b1c39030
irinazhibul <zhibulirina@gmail.com>
2023-01-23 03:37:06 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -37,8 +37,43 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+/*function decode(expr) {
+    let array = [];
+    array = convertStringTo10SymbolInElementsOfArray(expr)
+    let numberArray = array.map(element => element.replace(/^0+/, ''))
+    let morseArray = numberArray.map(el => el.replaceAll(11, '-').replaceAll(10, '.').replaceAll('**********', ' '))
+    let decodedArray = convertMorseArrayToDecodedArray(morseArray)
+    return decodedArray.join('')
+}
+
+function convertStringTo10SymbolInElementsOfArray(expr) {
+    let array = [];
+    let index = 0;
+    while (index < expr.length) {
+       array.push(expr.slice(index, index + 10));
+       index += 10;
+    }
+    return array
+}
+
+function convertMorseArrayToDecodedArray(array) {
+    let decodedArray = array.map(el => {
+        if (el === ' ') {
+            return ' '
+        } else {
+            return MORSE_TABLE[el]
+        }})
+    return decodedArray
+    }*/
+
 function decode(expr) {
-    // write your solution here
+    let array = [];
+    let decodedArray;
+    for (let i = 0; i < expr.length; i += 10) {
+       array.push(expr.slice(i, i + 10))
+       decodedArray = array.map(element => element.replace(/^0+/, '').replaceAll(11, '-').replaceAll(10, '.').replaceAll('**********', ' ')).map(el => el === ' ' ? ' ' :  MORSE_TABLE[el]);
+    }
+    return decodedArray.join('')
 }
 
 module.exports = {
